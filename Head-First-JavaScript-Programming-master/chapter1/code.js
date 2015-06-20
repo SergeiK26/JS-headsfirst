2d71923ad44d82c502cabc9dc430f88256901e41
SergeiK26 <s.kharchenko.q@gmail.com>
2015-06-20 16:38:34 +0300

    s
    s

--- Head-First-JavaScript-Programming-master/chapter1/code.js
+++ Head-First-JavaScript-Programming-master/chapter1/code.js
@@ -1,3 +1,4 @@
+scoops = 5;
 while (scoops > 0) {
 	document.write("Another scoop!<br>");
 	if (scoops < 3) {
@@ -8,4 +9,3 @@ while (scoops > 0) {
 	scoops = scoops - 1;
 }
 document.write("Life without ice cream isn't the same");
-
